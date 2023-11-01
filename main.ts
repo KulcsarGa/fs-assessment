9cf22894438e9c31e703bfb920520e30daa3a8b5
KulcsarGa <64829998+KulcsarGa@users.noreply.github.com>
2023-11-01 12:14:52 +0100

    test class in main

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log("hello")
+import FS from './models/classes/fs.ts'
+
+
+//Test with the given example
+const fs = new FS("/topdir")
+fs.store("filename1", "a very long string1")
+fs.store("filename2", "a very long string1")
+fs.store("filename3", "a very long string3")
+const result1 = fs.get("filename1")// gets 'a very long string1'
+const result2 = fs.get("filename2")// gets 'a very long string1'
+const result3 = fs.get("filename3")// gets 'a very long string3'
+
+
+console.log(fs)
+
+console.log("Content for filename1: " + result1)
+console.log("Content for filename2: " + result2)
+console.log("Content for filename3: " + result3)
