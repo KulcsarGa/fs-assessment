5ee952cac82960d68196a94dcf6f81941c074446
KulcsarGa <64829998+KulcsarGa@users.noreply.github.com>
2023-11-01 12:10:53 +0100

    update fs class with function definitions

--- models/classes/fs.ts
+++ models/classes/fs.ts
@@ -1,14 +1,36 @@
-import FSInterface from '../interfaces/fs'
+import FSInterface from "../interfaces/fs";
+import crypto from "crypto";
 
 class FS implements FSInterface {
+  directory: string;
+  fileData: { [filename: string]: string };
+  hashMap: { [hashedContent: string]: string };
 
-  store (filename: string, content: string): void {
-    console.log(filename)
-    console.log(content)
+  constructor(directory: string) {
+    this.directory = directory;
+    this.fileData = {};
+    this.hashMap = {};
   }
-  get (filename: string) : string{
-    return filename
+  store(filename: string, content: string): void {
+    const hashedContent = this.generateHash(content);
+    if (!this.hashMap[hashedContent]) {
+      this.hashMap[hashedContent] = content;
+    }
+    this.fileData[filename] = hashedContent;
+  }
+  get(filename: string): string {
+    if (this.fileData[filename]) {
+      const hashKey = this.fileData[filename];
+      const content = this.hashMap[hashKey];
+
+      return content;
+    }
+    return `There are no files with the filename ${filename}!` 
+  }
+
+  private generateHash(content: string) {
+    return crypto.createHash("md5").update(content).digest("hex");
   }
 }
 
-export default FS
+export default FS;
